faec6d97d73d907ce1924bbd8c0cd826b074996c
amerotz <amerotz@proton.me>
2024-06-17 11:06:21 +0200

    Update and rename folkrnn_inference.py to inference.py

--- inference.py
+++ inference.py
@@ -6,27 +6,29 @@ import pytorch_lightning.callbacks as plc
 import pytorch_lightning.loggers as pll
 
 from models.folkrnn import *
+from models.folktune_vae import *
+
 from dataset import *
 import random
 
 torch.multiprocessing.set_sharing_strategy("file_system")
 torch.manual_seed(0)
 random.seed(0)
+seed = 2394
 
 # load dataset
 dataset = FolkDataset(data_file="data/data_v2", max_sequence_length=256)
 
-dataset_train, dataset_test = tud.random_split(dataset, [0.95, 0.05])
-
+torch.manual_seed(seed)
+random.seed(seed)
+'''
+########## FOLKRNN ########## 
 # load model
 model = FolkRNN.load_from_checkpoint(
     "folkRNN/version_X/checkpoints/epoch=20-step=6636.ckpt"
 )
 model.eval()
 
-seed = 2394
-torch.manual_seed(seed)
-random.seed(seed)
 gen = model.inference(
     dataset.sos_idx,
     dataset.eos_idx,
@@ -34,6 +36,26 @@ gen = model.inference(
     mode="topp",
     temperature=1,
     PK=0.9,
+)
+########## ########## ########## 
+'''
+########## FOLKTUNE VAE ########## 
+
+# load model
+model = FolktuneVAE.load_from_checkpoint(
+    "foltune_vae/version_X/checkpoints/epoch=20-step=6636.ckpt"
+)
+model.eval()
+
+gen = model.inference(
+    dataset.sos_idx,
+    dataset.eos_idx,
+    max_len=args.max_sequence_length,
+    mode=args.mode,
+    temperature=args.temperature,
+    PK=args.pk,
+    prompt=prompt,
+    z=z,
 )
 
 
